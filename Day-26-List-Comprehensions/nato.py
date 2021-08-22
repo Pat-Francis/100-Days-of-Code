ae3d429493f61ea51a32a5c2f9b0bae3254d54f1
Pat-Francis <github@pmfrancis.com>
2021-08-22 21:09:08 +0100

    Day 26 completed

---
+++ Day-26-List-Comprehensions/nato.py
@@ -0,0 +1,16 @@
+import pandas
+# Load CSV into Pandas Dataframe
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+# Iterates through data and appends to data_dict in the format 'A': 'Alfa'
+data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+
+def get_nato():
+    # Ask user for word input
+    word = input("Please input your word: ")
+    # Iterate through word letter by letter and add corresponding code word
+    nato_phonetic = [data_dict[letter] for letter in word.upper()]
+    print(nato_phonetic)
+
+
+get_nato()
