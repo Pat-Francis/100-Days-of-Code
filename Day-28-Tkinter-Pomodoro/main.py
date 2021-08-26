e3217952e9d1f6b1036e6e2826deceaa972a048f
Pat-Francis <github@pmfrancis.com>
2021-08-26 21:25:52 +0100

    Day 28 UI complete

--- Day-28-Tkinter-Pomodoro/main.py
+++ Day-28-Tkinter-Pomodoro/main.py
@@ -26,6 +26,22 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+# Create the 'Timer' label and configure
+timer_label = Label(text="Timer", font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
+
+# Create check mark label and configure
+check_label = Label(text="✔", font=(FONT_NAME, 10), fg=GREEN, bg=YELLOW)
+check_label.grid(column=1, row=3)
+
+# Create Start button and configure
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+# Create Reset button and configure
+start_button = Button(text="Reset")
+start_button.grid(column=2, row=2)
 
 window.mainloop()
