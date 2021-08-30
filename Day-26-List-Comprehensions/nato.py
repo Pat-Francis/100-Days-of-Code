fcbcf019ec92862f9f5ee97b2da89163b7c2955f
Pat-Francis <github@pmfrancis.com>
2021-08-30 10:48:09 +0100

    Day 30 Added Exception Handling to nato.py

--- Day-26-List-Comprehensions/nato.py
+++ Day-26-List-Comprehensions/nato.py
@@ -8,9 +8,14 @@ data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 def get_nato():
     # Ask user for word input
     word = input("Please input your word: ")
-    # Iterate through word letter by letter and add corresponding code word
-    nato_phonetic = [data_dict[letter] for letter in word.upper()]
-    print(nato_phonetic)
+    try:
+        # Iterate through word letter by letter and add corresponding code word
+        nato_phonetic = [data_dict[letter] for letter in word.upper()]
+    except KeyError:
+        print("Please use letters only")
+        get_nato()
+    else:
+        print(nato_phonetic)
 
 
 get_nato()
