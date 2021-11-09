c8e150fc499d9b3dcd2dc3e927f9ff6b794c1a92
Pat-Francis <github@pmfrancis.com>
2021-11-09 12:42:30 +0000

    Day 46 Step 3 Completed

--- Day-46-Music-Time-Machine/main.py
+++ Day-46-Music-Time-Machine/main.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
 SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
@@ -26,12 +28,26 @@ song_titles = [song.getText() for song in all_songs]
 # Set scope for spotify access token
 scope = "playlist-modify-private"
 
-# Auth with Spotify, adds the .cache file to the directory which hols the Spotify access token
+# Auth with Spotify, adds the .cache file to the directory which holds the Spotify access token
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
                                                client_secret=SPOTIPY_CLIENT_SECRET,
                                                redirect_uri=SPOTIPY_REDIRECT_URI,
                                                scope=scope))
 
-# Pull user info from SPotify and print current users display name
+# Pull user info from Spotify and print current users display id
 user_info = sp.current_user()
-print(user_info.get("display_name"))
+print(user_info.get("id"))
+
+song_URIs = []
+year = chosen_date.split("-")[0]
+# Iterate through the list of song titles and get the corresponding Spotify track URI, if the track exists
+for song in song_titles:
+    search_result = sp.search(q=f"track: {song} year: {year}", type="track")
+
+    try:
+        uri = search_result["tracks"]["items"][0]["uri"]
+        song_URIs.append(uri)
+    except IndexError:
+        print(f"Could not find '{song}' on Spotify.  Skipped.")
+
+pp.pprint(song_URIs)
