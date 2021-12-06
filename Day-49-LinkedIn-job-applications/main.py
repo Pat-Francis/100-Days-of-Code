ca303fe185f9cae5d673a6efa887fcc8569296f2
Pat-Francis <github@pmfrancis.com>
2021-12-06 21:51:38 +0000

    Day 49 Job iteration and saves completed

--- Day-49-LinkedIn-job-applications/main.py
+++ Day-49-LinkedIn-job-applications/main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import os
+import time
 
 linkedin_username = os.getenv("LINKEDIN_USERNAME")
 linkedin_password = os.getenv("LINKEDIN_PASSWORD")
@@ -22,5 +23,20 @@ enter_username.send_keys(linkedin_username)
 enter_password = driver.find_element(By.ID, "password")
 enter_password.send_keys(linkedin_password)
 enter_password.send_keys(Keys.ENTER)
+time.sleep(5)
 
-# driver.quit()
+element_class_name = "jobs-search-results__list-item"
+jobs = driver.find_elements(By.CLASS_NAME, element_class_name)
+
+for index in range(len(jobs)):
+    # Refresh jobs every iteration - otherwise throws StaleElementReferenceException as items are no longer in
+    # Document Object Model (DOM).  DOM changes every interaction with web page?
+    jobs = driver.find_elements(By.CLASS_NAME, element_class_name)
+
+    jobs[index].click()
+    time.sleep(3)
+
+    save_button = driver.find_element(By.CLASS_NAME, "jobs-save-button")
+    save_button.click()
+
+driver.quit()
