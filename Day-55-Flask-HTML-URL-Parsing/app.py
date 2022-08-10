dfea24d50ec60dc9a489e23db9fc0585820a8aad
Pat-Francis <github@pmfrancis.com>
2022-08-10 20:18:51 +0100

    Day 55 started

---
+++ Day-55-Flask-HTML-URL-Parsing/app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+def make_bold(function):
+    def wrap_text():
+        return f"<b>{function()}</b>"
+    return wrap_text
+
+
+def make_italic(function):
+    def wrap_text():
+        return f"<i>{function()}</i>"
+    return wrap_text
+
+
+def make_underlined(function):
+    def wrap_text():
+        return f"<u>{function()}</u>"
+    return wrap_text
+
+
+@app.route('/bye')
+@make_bold
+@make_italic
+@make_underlined
+def bye():
+    return 'Bye!'
