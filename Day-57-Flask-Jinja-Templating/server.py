227eb50aadc7d0f24bd4653916d41e1a2e5dc423
Pat-Francis <github@pmfrancis.com>
2022-08-17 19:56:32 +0100

    Day 57 - Part 3 completed

--- Day-57-Flask-Jinja-Templating/server.py
+++ Day-57-Flask-Jinja-Templating/server.py
@@ -25,6 +25,13 @@ def guess(name):
 
     return render_template('guess.html', year=current_year, name=name.title(), gender=gender, age=age)
 
+@app.route('/blog')
+def blog():
+    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+    blog_response = requests.get(url=blog_url)
+    all_posts = blog_response.json()
+
+    return render_template('blog.html', posts=all_posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
