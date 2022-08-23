9d685865d308ca82ea6d0768f316f8e60be55767
Pat-Francis <github@pmfrancis.com>
2022-08-23 19:51:55 +0100

    Day 63 - SQLAlchemy

---
+++ Day-63-SQLAlchemy/main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), unique=True, nullable=False)
+    rating = db.Column(db.String(4), unique=True, nullable=False)
+
+
+db.create_all()
+
+harry_potter = Books(id=1, title="Harry Potter", author="J. K. Rowling", rating="9.3")
+db.session.add(harry_potter)
+db.session.commit()
